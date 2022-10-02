c8d35c057cb117b502bb8832cf4de5d5ed67c27e
Lucas3110 <lucas.arg.rodriguez@gmail.com>
2022-10-02 11:21:36 -0300

    falta arreglar bien los colores pero todo andando form donaciones

--- public/js/donaciones.js
+++ public/js/donaciones.js
@@ -78,7 +78,7 @@ function validate(){
                 }
     const donation = document.getElementById("donation");
     donation.innerHTML = "";
-    donation.innerHTML = "$"+donations+" won in the last 24 hours";
+    donation.innerHTML = "$"+donations+" recaudado en las últimas 24 horas";
 }else{
   if(!cc){
     const ccError = document.getElementById("cc");
