dcce058004500d4cc2e5298b3c93bd2d09d50148
Lucas3110 <lucas.arg.rodriguez@gmail.com>
2022-10-22 17:24:35 -0300

    Proyecto Reutiliza terminado

--- public/js/contacto.js
+++ public/js/contacto.js
@@ -1,30 +1,29 @@
-    const $form = document.querySelector('#form')
-  
-    $form.addEventListener('submit', handleSubmit)
-  
-    async function handleSubmit(event) {
-      event.preventDefault()
-      const form = new FormData(this)
-      const response = await fetch(this.action, {
-        method: this.method,
-        body: form,
-        headers: {
-          'Accept': 'application/json'
-        }
-      })
-      if (response.ok) {
-        this.reset()
-        alert('Gracias por contactarte con nostros, te responderemos a la brevedad')
-      }
-    }
+const $form = document.querySelector('#form')
 
-    function validateForm() {
-      var a = document.forms["Form"]["nombre"].value;
-      var b = document.forms["Form"]["email"].value;
-      var c = document.forms["Form"]["mensaje"].value;     
-      if (a == null || a == "", b == null || b == "", c == null || c == "") {
-        alert("Por favor completa todos los campos");
-        return false;
-      }
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      'Accept': 'application/json'
     }
-  
+  })
+  if (response.ok) {
+    this.reset()
+    alert('Gracias por contactarte con nostros, te responderemos a la brevedad')
+  }
+}
+
+function validateForm() {
+  var a = document.forms["Form"]["nombre"].value;
+  var b = document.forms["Form"]["email"].value;
+  var c = document.forms["Form"]["mensaje"].value;
+  if (a == null || a == "", b == null || b == "", c == null || c == "") {
+    alert("Por favor completa todos los campos");
+    return false;
+  }
+}
--- public/js/donaciones.js
+++ public/js/donaciones.js
@@ -1,14 +1,14 @@
 let donations = 0;
 let selected = false;
 let selId = 0;
-//Onclick function to highlight selected donation amount
-function setValue(element){
+//Onclick function para resaltar monto seleccionado
+function setValue(element) {
   let val = element;
-  if(!selected){
+  if (!selected) {
     val.style.background = "#82d14b";
     selId = val;
     selected = true;
-  }else{
+  } else {
     selId.style.background = "#cececc"; //fondo dsp de seleccionar
     val.style.background = "#82d14b";
     selId = val;
@@ -21,80 +21,80 @@ function validateCVC() {
   return matchExists(cvcreg, cvc.value);
 }
 
-function matchExists(regex, value){
+function matchExists(regex, value) {
   let mat = value.match(regex);
   const matchExists = mat != null;
   return (matchExists && mat[0].length == value.length);
 }
-function validateCC(){
+function validateCC() {
   const cc = document.getElementById("cc");
   const ccreg = /\d{16}/;
   return matchExists(ccreg, cc.value);
 }
-function validateName(){
+function validateName() {
   const name = document.getElementById("name");
   const namereg = /[a-zA-Z]*\s?[a-zA-Z]*/;
   return matchExists(namereg, name.value);
 }
-function validateDate(){
+function validateDate() {
   const date = document.getElementById("date");
   const datereg = /(1[0-2]|0[1-9]|\d)\/(1[7-9]|[2-9]\d)/;
   return matchExists(datereg, date.value);
 }
-function validate(){
-   const indicator = document.getElementsByClassName("form-indicator")[0];
+function validate() {
+  const indicator = document.getElementsByClassName("form-indicator")[0];
   const cvc = validateCVC();
   const cc = validateCC();
   const date = validateDate();
   const name = validateName();
-  
+
   const allAreValid = cvc && cc && date && name;
-  
-  if(allAreValid){
-    switch(Number(selId.id)) {
-    case 1:
-        donations+=5;
+
+  if (allAreValid) {
+    switch (Number(selId.id)) {
+      case 1:
+        donations += 5;
         break;
-    case 2:
-        donations+=10;
+      case 2:
+        donations += 10;
         break;
-    case 3:
-        donations+=20;
+      case 3:
+        donations += 20;
         break;
-    case 4:
-        donations+=40;
+      case 4:
+        donations += 40;
         break;
-    case 5:
-        donations+=100;
+      case 5:
+        donations += 100;
         break;
-    case 6:
-        donations+=250;
+      case 6:
+        donations += 250;
         break;
-    case 7:
-        donations+=1000;
+      case 7:
+        donations += 1000;
         break;
-    default:
+      default:
         break;
-                }
+    }
     const donation = document.getElementById("donation");
     donation.innerHTML = "";
-    donation.innerHTML = "$"+donations+" recaudado en las últimas 24 horas";
-}else{
-  if(!cc){
-    const ccError = document.getElementById("cc");
-    ccError.style.color = "red";
+    donation.innerHTML = "$" + donations + " recaudado en las últimas 24 horas";
+  } else {
+    if (!cc) {
+      const ccError = document.getElementById("cc");
+      ccError.style.color = "red";
+    }
+    if (!cvc) {
+      const cvcError = document.getElementById("cvc");
+      cvcError.style.color = "red";
+    }
+    if (!date) {
+      const dateError = document.getElementById("date");
+      dateError.style.color = "red";
+    }
+    if (!name) {
+      const nameError = document.getElementById("name");
+      nameError.style.color = "red";
+    }
   }
-  if(!cvc){
-    const cvcError = document.getElementById("cvc");
-    cvcError.style.color = "red";
-  }
-  if(!date){
-    const dateError = document.getElementById("date");
-    dateError.style.color = "red";
-  }
-  if(!name){
-    const nameError = document.getElementById("name");
-    nameError.style.color = "red";
-  }
-}
 }
